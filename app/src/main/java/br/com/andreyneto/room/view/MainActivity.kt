a622f821b1747de24852c3571d82936c70ff990e
Andrey Neto <andrey.neto@going2.com.br>
2019-02-09 11:58:36 -0200

    Update MainActivity.kt

--- app/src/main/java/br/com/andreyneto/room/view/MainActivity.kt
+++ app/src/main/java/br/com/andreyneto/room/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.andreyneto.room.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -7,16 +8,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import br.com.andreyneto.room.R
 import br.com.andreyneto.room.entities.Word
 import br.com.andreyneto.room.viewmodel.WordViewModel
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val NEW_WORD_ACTIVITY_REQUEST_CODE = 1
+    }
+
+    lateinit var wordViewModel: WordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +32,28 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
+        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
         wordViewModel.allWords.observe(this, Observer {
             adapter.words = it
         })
 
         fab.setOnClickListener { view ->
-            startActivity<NewWordActivity>()
+            val i = Intent(this, NewWordActivity::class.java)
+            startActivityForResult(i, NEW_WORD_ACTIVITY_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE &&
+            resultCode == RESULT_OK) {
+            data?.let {
+                val word =
+                    Word(it.getStringExtra(NewWordActivity.EXTRA_REPLY))
+                wordViewModel.insert(word)
+            }
+        } else {
+            toast(R.string.empty_not_saved)
         }
     }
 
@@ -48,7 +68,11 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_clear -> {
+                wordViewModel.deleteAll()
+                toast(R.string.cleared)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
