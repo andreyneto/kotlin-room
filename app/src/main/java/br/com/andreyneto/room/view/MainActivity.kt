c4bb869b78d06cc209f68458a2b9af679d1fe1d2
Andrey Neto <andrey.neto@going2.com.br>
2019-02-09 11:45:51 -0200

    Update MainActivity.kt

--- app/src/main/java/br/com/andreyneto/room/view/MainActivity.kt
+++ app/src/main/java/br/com/andreyneto/room/view/MainActivity.kt
@@ -9,10 +9,12 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.andreyneto.room.R
+import br.com.andreyneto.room.entities.Word
 import br.com.andreyneto.room.viewmodel.WordViewModel
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,18 +23,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
         val adapter = WordListAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
         val wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
         wordViewModel.allWords.observe(this, Observer {
             adapter.words = it
         })
+
+        fab.setOnClickListener { view ->
+            startActivity<NewWordActivity>()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
