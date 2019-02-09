6f7c7a6ba9bc761ff8618ed2b3c5d09a3a18d12c
Andrey Neto <andrey.neto@going2.com.br>
2019-02-09 10:59:25 -0200

    Create WordListAdapter.kt

---
+++ app/src/main/java/br/com/andreyneto/room/view/WordListAdapter.kt
@@ -0,0 +1,38 @@
+package br.com.andreyneto.room.view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.com.andreyneto.room.R
+import br.com.andreyneto.room.entities.Word
+
+class WordListAdapter(context: Context) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return WordViewHolder(itemView)
+    }
+
+    override fun getItemCount() = words.size
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        if(words.isEmpty()) {
+            holder.wordItemView.text = "No Words"
+        } else {
+            holder.wordItemView.text = words[position].word
+        }
+    }
+
+    private val inflater = LayoutInflater.from(context)
+    var words: List<Word> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class WordViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val wordItemView = itemView.findViewById<TextView>(R.id.textView)
+    }
+}
