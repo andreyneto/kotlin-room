5c7b52a73d80972c50ab50965443f4342466a078
Andrey Neto <andrey.neto@going2.com.br>
2019-02-09 11:24:37 -0200

    Update WordRoomDatabase.kt

--- app/src/main/java/br/com/andreyneto/room/db/WordRoomDatabase.kt
+++ app/src/main/java/br/com/andreyneto/room/db/WordRoomDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import br.com.andreyneto.room.dao.WordDao
 import br.com.andreyneto.room.entities.Word
+import org.jetbrains.anko.doAsync
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordRoomDatabase : RoomDatabase() {
@@ -13,6 +15,22 @@ abstract class WordRoomDatabase : RoomDatabase() {
 
     companion object {
         private var instance: WordRoomDatabase? = null
+
+        private val roomDatabaseCallback = object : RoomDatabase.Callback() {
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                instance?.let {
+                    doAsync {
+                        val dao = it.wordDao()
+                        dao.deleteAll()
+                        val word = Word("Movile")
+                        dao.insert(word)
+                        val word2 = Word("Next")
+                        dao.insert(word2)
+                    }
+                }
+            }
+        }
 
         fun getDatabase(context: Context) : WordRoomDatabase {
             if (instance == null) {
